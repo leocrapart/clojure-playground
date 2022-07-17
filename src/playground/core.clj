f62d553b5fe07633eb2a9bd9cd1cf8517fc428db
Leo Crapart <leocrapart@yahoo.fr>
2022-07-17 23:56:38 +0200

    euler 31

--- src/playground/core.clj
+++ src/playground/core.clj
@@ -465,3 +465,158 @@ href-lines
 (reduce + [2 4])
 
 (dedupe [0 0 0 0 1 1 2 1 2 11 "11" "11" "11"])
+
+
+
+
+
+
+(type \h)
+
+(def ascii-value 
+  {\@ 64
+   \A 65
+   \B 66
+   \C 67
+   \D 68
+   \E 69
+   \F 70
+   \G 71
+   \H 72
+   \I 73
+   \J 74
+   \K 75
+   \L 76
+   \M 77
+   \N 78
+   \O 79
+   \P 80
+   \Q 81
+   \R 82
+   \S 83
+   \T 84
+   \U 85
+   \V 86
+   \W 87
+   \X 88
+   \Y 89
+   \Z 90 
+   \a 97
+   \b 98
+   \c 99
+   \d 100
+   \e 101
+   \f 102
+   \g 103
+   \h 104
+   \i 105
+   \j 106
+   \k 107
+   \l 108
+   \m 109
+   \n 110
+   \o 111
+   \p 112
+   \q 113
+   \r 114
+   \s 115
+   \t 116
+   \u 117
+   \v 118
+   \w 119
+   \x 120
+   \y 121
+   \z 122
+   \space 32
+  })
+
+
+(defn binary [number]
+  (Long/toString number 2))
+
+(binary 2)
+
+(defn hash [str]
+  (map binary
+    (map ascii-value str)))
+
+
+
+  (hash "hello world")
+
+
+
+
+
+
+
+
+;; e31
+
+(def value 
+  {"1p" 0.01
+   "2p" 0.02
+   "5p" 0.05
+   "10p" 0.10
+   "20p" 0.20
+   "50p" 0.50
+   "1l" 1.00
+   "2l" 2.00
+  })
+
+
+(value "1l")
+
+
+
+
+
+(defn coin-pair-value [coin-pair]
+  (let [number (coin-pair 0)
+        coin (coin-pair 1)]
+    (* number (value coin))))
+
+(coin-pair-value [2 "1l"])
+
+(defn coin-pairs-sum [coin-pairs]
+  (reduce +
+    (map coin-pair-value coin-pairs)))
+
+(coin-pairs-sum [[2 "2l"] [1 "1l"]])
+
+
+(defn is-2l-sum [coin-pairs]
+  (if (= (value "2l") (coin-pairs-sum coin-pairs))
+    true
+    false))
+
+(is-2l-sum [[1 "2l"] [0 "1l"]])
+
+
+(def max
+  {"1p" 200
+   "2p" 100
+   "5p" 40
+   "10p" 20
+   "20p" 10
+   "50p" 4
+   "1l" 2
+   "2l" 1 
+    })
+
+
+(def all-coin-pairs-possible
+  [[[1 "2l"] [2 "1l"] [4 "50p"] [10 "20p"] [20 "10p"] [40 "5p"] [100 "2p"] [200 "1p"]]
+  ])
+
+
+()
+
+
+
+
+
+
+
+
+
